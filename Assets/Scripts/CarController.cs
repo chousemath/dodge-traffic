d23ce68c56365534c7017a7d66990b0e8e5ca425
Joseph Sungpil Choi <chousemath@gmail.com>
2018-03-03 18:04:21 +0900

    Add a little bit of physics details to the car

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -4,19 +4,26 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 	public float moveSpeed = -5f;
+	private float yOrigin;
+	private float timer = 0;
 	Rigidbody2D rb2d;
 
 	// Use this for initialization
 	void Awake () {
 		rb2d = GetComponent<Rigidbody2D>();
+		yOrigin = rb2d.position.y;
+		gameObject.GetComponent<Renderer>().material.color = Color.red;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
 	}
 
 	void FixedUpdate () {
-		rb2d.MovePosition(new Vector2(rb2d.position.x + moveSpeed * Time.fixedDeltaTime, rb2d.position.y));
+		timer += 5 * Time.fixedDeltaTime;
+		rb2d.MovePosition(new Vector2(
+			rb2d.position.x + moveSpeed * Time.fixedDeltaTime,
+			yOrigin + 0.07f * Mathf.Sin(timer)
+		));
 	}
 }
